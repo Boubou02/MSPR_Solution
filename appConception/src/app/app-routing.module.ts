b51e5d636480753a94eaadddd0dca7e18aae9dc2
Kellner <maximek.travail@gmail.com>
2019-03-08 11:48:11 +0100

    Branch _ Convert angular to Ionic project

--- appConception/src/app/app-routing.module.ts
+++ appConception/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PlanComponent } from './plan/plan.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'plan', component: PlanComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
